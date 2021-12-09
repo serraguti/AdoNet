0d8e6640f1dfeda39be4003b867183f6742263d2
serraguti <pacoserranox@gmail.com>
2021-12-09 14:05:49 +0100

    commit

--- AdoNet/Form09UpdateInsertDoctor.cs
+++ AdoNet/Form09UpdateInsertDoctor.cs
@@ -8,6 +8,31 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+#region PROCEDIMIENTO ALMACENADO
+//CREATE PROCEDURE ACTUALIZARDOCTOR
+//(@IDDOCTOR INT, @APELLIDO NVARCHAR(50)
+//, @ESPECIALIDAD NVARCHAR(50), @SALARIO INT
+//, @HOSPITAL INT)
+//AS
+//	declare @numdoctors int
+//	select @numdoctors = count(doctor_no) from doctor
+//	where DOCTOR_NO = @IDDOCTOR
+//	if (@numdoctors = 0)
+//	begin
+//		insert into doctor values (@HOSPITAL, @IDDOCTOR
+//		, @APELLIDO, @ESPECIALIDAD, @SALARIO)
+//		print 'Doctor insertado'
+//	end
+//	else
+//	begin
+//		update doctor set APELLIDO = @APELLIDO, ESPECIALIDAD = @ESPECIALIDAD
+//		, salario = @salario, HOSPITAL_COD = @HOSPITAL
+//		where DOCTOR_NO = @IDDOCTOR
+//		print 'Doctor actualizado'
+//	end
+//GO
+#endregion
+
 namespace AdoNet
 {
     public partial class Form09UpdateInsertDoctor : Form
